e799dfb6af20e96467b0241430229aad7a7d1af7
Your Name <your.email@example.com>
2026-06-24 21:06:59 +0200

    feat: add ProductService and IProductService

---
+++ Infrastructure/Service/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Service
+{
+   public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product?> GetProductByIdAsync(int id);
+        Task<int> CreateProductAsync(Product product); 
+        Task<bool> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(int id); 
+        Task<IEnumerable<Product>> GetProductsByCategoryAsync(int categoryId);
+        Task<Product?> GetProductDetailsAsync(int id);
+        Task<IEnumerable<Product>> GetLatestProductsAsync(int count);
+        Task<IEnumerable<Product>> SearchProductsAsync(string searchTerm);
+    }
+}
---
+++ Infrastructure/Service/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using Infrastructure.Models;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Service
+{
+    public class ProductService:IProductService
+    {
+        private readonly IProductRepository _repo;
+        public ProductService(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+            => await _repo.GetAllProductsAsync();
+        public async Task<Product?> GetProductByIdAsync(int id)
+            => await _repo.GetProductByIdAsync(id);
+        public async Task<int> CreateProductAsync(Product product)
+        {
+            if(await _repo.ExistsProductAsync(product.ProductName))
+            {
+                throw new Exception($"Product with ID {product.ProductName} already exists.");
+            }
+            return await _repo.AddProductAsync(product);
+        }
+        public async Task<bool> UpdateProductAsync(Product product)
+        {
+            return await _repo.UpdateProductAsync(product);
+        }
+        public async Task<bool> DeleteProductAsync(int id)
+        {
+            return await _repo.DeleteProductAsync(id);
+        }
+        public async Task<IEnumerable<Product>> GetProductsByCategoryAsync(int categoryId)
+            =>  await _repo.GetProductsByCategoryIdAsync(categoryId);
+        public async Task<Product?> GetProductDetailsAsync(int id)
+            => await _repo.GetProductDetailsByIdAsync(id);
+        public async Task<IEnumerable<Product>> GetLatestProductsAsync(int count)
+        => await _repo.GetLatestProductsAsync(count);
+
+        public async Task<IEnumerable< Product>> SearchProductsAsync(string searchTerm)
+            => await _repo.SearchProductsAsync(searchTerm);
+    }
+}
